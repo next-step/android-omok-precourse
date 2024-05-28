636454e5664c4eca3d54f73a4476e3ed40f92216
KSC <ksc1008@naver.com>
2024-05-28 17:56:33 +0900

    feat($main): add functions to bind table layout with board.
    Now a board data is stored in 2-dimensional list and added some functions to bind table layout with the list.

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -8,16 +8,37 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private val boardGridCells:MutableList<MutableList<ImageView>> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val table = findViewById<TableLayout>(R.id.board)
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
+        bindBoardGridCellsWithTable(table)
+    }
+
+    private fun bindBoardGridCellsWithTable(table: TableLayout) {
+        table
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, row -> initiateBoardGridRow(index, row) }
+    }
+
+    private fun initiateBoardGridRow(rowIndex: Int, tableRow: TableRow) {
+        boardGridCells.add(mutableListOf())
+        tableRow.children.filterIsInstance<ImageView>().forEachIndexed { index, view ->
+            boardGridCells.last().add(view)
+            view.setOnClickListener { clickTable(index, rowIndex) }
+        }
+    }
+
+    private fun clickTable(x: Int, y:Int) {
+        if((x+y) % 2 == 0){
+            boardGridCells[y][x].setImageResource(R.drawable.white_stone)
+        }
+        else{
+            boardGridCells[y][x].setImageResource(R.drawable.black_stone)
+        }
     }
 }
