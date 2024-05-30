3d948c98f2412ce37ca89905961761b0f9055acf
changukLee <ckddnr5527@gmail.com>
2024-05-30 23:42:39 +0900

    feat: add place stone feature
    위치 확인 후 '두기' 버튼을 누르면 돌이 놓여지고 차례가 넘어가도록 기능 추가

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,31 +1,41 @@
 package nextstep.omok
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
     var isBlackTurn = true
+    var readyToPlace = false
     lateinit var board: TableLayout
     lateinit var blackTurnImageView: ImageView
     lateinit var whiteTurnImageView: ImageView
+    lateinit var placeStoneBtn: Button
+    lateinit var cellToPlace: ImageView
+    lateinit var prevCellToPlace: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initVariables()
         setOnClickListenerForStones()
+        setOnClickListenerForPlaceStoneBtn()
     }
+
 
     private fun initVariables() {
         board = findViewById(R.id.board)
         blackTurnImageView = findViewById(R.id.black_turn_image)
         whiteTurnImageView = findViewById(R.id.white_turn_image)
+        placeStoneBtn = findViewById(R.id.place_stone_btn)
     }
 
     private fun setOnClickListenerForStones() {
@@ -34,22 +44,43 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view ->
-                view.setOnClickListener {
-                    placeStone(view)
+            .forEach { cell ->
+                cell.setOnClickListener {
+                    previewStone(cell)
                 }
             }
     }
 
-    private fun placeStone(viewToPlace: ImageView) {
+    private fun previewStone(cell: ImageView) {
+        if (readyToPlace) {
+            prevCellToPlace.setImageDrawable(null)
+        }
+        if (cell.drawable == null) {
+            cell.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dashed_stone))
+            readyToPlace = true
+            cellToPlace = cell
+            prevCellToPlace = cell
+        }
+    }
+
+    private fun setOnClickListenerForPlaceStoneBtn() {
+        placeStoneBtn.setOnClickListener {
+            if (readyToPlace) {
+                placeStone()
+                readyToPlace = false
+            } else {
+                Toast.makeText(this, "먼저 돌을 둘 곳을 클릭하세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun placeStone() {
         if (isBlackTurn) {
-            viewToPlace.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dashed_stone))
-            //viewToPlace.setImageResource(R.drawable.black_stone)
+            cellToPlace.setImageResource(R.drawable.black_stone)
             blackTurnImageView.visibility = View.INVISIBLE
             whiteTurnImageView.visibility = View.VISIBLE
         } else {
-            viewToPlace.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dashed_stone))
-            //viewToPlace.setImageResource(R.drawable.white_stone)
+            cellToPlace.setImageResource(R.drawable.white_stone)
             whiteTurnImageView.visibility = View.INVISIBLE
             blackTurnImageView.visibility = View.VISIBLE
         }
