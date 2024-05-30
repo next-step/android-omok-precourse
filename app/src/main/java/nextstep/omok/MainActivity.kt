40be63c6f6b65b6ffb7d3e0b461dbd6b91fdbc03
changukLee <ckddnr5527@gmail.com>
2024-05-30 23:57:04 +0900

    comment: add function comments

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -30,7 +30,13 @@ class MainActivity : AppCompatActivity() {
         setOnClickListenerForPlaceStoneBtn()
     }
 
-
+    /**
+     * 변수와 뷰를 초기화.
+     * - `board`: 오목 보드 뷰.
+     * - `blackTurnImageView`: 흑돌 차례를 나타내는 이미지 뷰.
+     * - `whiteTurnImageView`: 백돌 차례를 나타내는 이미지 뷰.
+     * - `placeStoneBtn`: 돌을 보드에 두는 버튼.
+     */
     private fun initVariables() {
         board = findViewById(R.id.board)
         blackTurnImageView = findViewById(R.id.black_turn_image)
@@ -38,6 +44,11 @@ class MainActivity : AppCompatActivity() {
         placeStoneBtn = findViewById(R.id.place_stone_btn)
     }
 
+
+    /**
+     * 보드의 각 칸(이미지 뷰)에 클릭 리스너 설정.
+     * 칸을 클릭하면 해당 칸이 맞는 지 미리 보여주는 동작 수행.
+     */
     private fun setOnClickListenerForStones() {
         board
             .children
@@ -51,6 +62,13 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    /**
+     * 칸을 클릭하면 해당 칸이 맞는 지 미리 보여주는 동작 수행.
+     * 놓을 위치를 변경 하고자 할 땐 이전 칸의 미리 보기 삭제.
+     * 칸에 돌이 없을 때만 동작.
+     *
+     * @param cell 클릭한 칸을 나타내는 이미지 뷰
+     */
     private fun previewStone(cell: ImageView) {
         if (readyToPlace) {
             prevCellToPlace.setImageDrawable(null)
@@ -63,6 +81,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    /**
+     * '두기' 버튼에 대한 클릭 리스너 설정.
+     * 돌을 둘 칸을 선택한 경우 돌을 둠.
+     * 돌을 둘 칸을 선택하지 않은 경우 안내 메시지를 Toast.
+     */
     private fun setOnClickListenerForPlaceStoneBtn() {
         placeStoneBtn.setOnClickListener {
             if (readyToPlace) {
@@ -74,6 +98,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 차례에 맞게 선택한 칸에 돌을 두는 함수.
+     * 돌을 두고 나면 차례를 바꿈.
+     */
     private fun placeStone() {
         if (isBlackTurn) {
             cellToPlace.setImageResource(R.drawable.black_stone)
