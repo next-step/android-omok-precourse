d9d5abe8869846bd664b96c68bb2fdb6e5bdc1e4
changukLee <ckddnr5527@gmail.com>
2024-05-30 22:28:35 +0900

    feat: add switching turn feature
    - 흑돌과 백돌을 번갈아서 놓을 수 있도록 함
    - 차례가 바뀔 때마다 차례 표시 UI가 바뀌도록 함

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.omok
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -8,16 +9,48 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    var isBlackTurn = true
+    lateinit var board: TableLayout
+    lateinit var blackTurnImageView: ImageView
+    lateinit var whiteTurnImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
+        initVariables()
+        setOnClickListenerForStones()
+    }
+
+    private fun initVariables() {
+        board = findViewById(R.id.board)
+        blackTurnImageView = findViewById(R.id.black_turn_image)
+        whiteTurnImageView = findViewById(R.id.white_turn_image)
+    }
+
+    private fun setOnClickListenerForStones() {
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view ->
+                view.setOnClickListener {
+                    placeStone(view)
+                }
+            }
     }
+
+    private fun placeStone(viewToPlace: ImageView) {
+        if (isBlackTurn) {
+            viewToPlace.setImageResource(R.drawable.black_stone)
+            blackTurnImageView.visibility = View.INVISIBLE
+            whiteTurnImageView.visibility = View.VISIBLE
+        } else {
+            viewToPlace.setImageResource(R.drawable.white_stone)
+            whiteTurnImageView.visibility = View.INVISIBLE
+            blackTurnImageView.visibility = View.VISIBLE
+        }
+        isBlackTurn = !isBlackTurn
+    }
+
 }
