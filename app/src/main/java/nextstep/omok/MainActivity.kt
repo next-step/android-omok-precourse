38d533783e880afca7617435d61a3e96701f1704
jmc98 <jmc0820@naver.com>
2024-05-31 21:02:24 +0900

    Feat(MainActivity.kt) : 추가 setOnClickListener
    보드 클릭 시 onStonePlaced 호출하고, 뷰 태그에 세로와 가로 좌표 저장

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -6,18 +6,31 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // 오목 보드
+    private lateinit var board: Array<Array<ImageView?>>
+    // 15x15 보드 사이즈
+    private val boardSize = 15
+    // 현재 플레이어(black or white)
+    private var currentPlayer = "black"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+        val tableLayout = findViewById<TableLayout>(R.id.board)
+        board = Array(boardSize) { arrayOfNulls(boardSize) }
+        var rowIndex = 0 // 세로
+        tableLayout.children.filterIsInstance<TableRow>().forEach { row ->
+                var colIndex = 0 // 가로
+                row.children.filterIsInstance<ImageView>().forEach { view ->
+                        view.setOnClickListener { onStonePlaced(view) } // 클릭 시 돌 놓기
+                        view.tag = "$rowIndex,$colIndex"  // 태그에 세로, 가로 설정
+                        board[rowIndex][colIndex] = view
+                        colIndex++ }
+                rowIndex++ }
     }
+
+
 }
