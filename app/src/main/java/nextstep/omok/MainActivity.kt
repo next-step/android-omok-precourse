94fc89de9e6f01a0dd7310ea4269909e7fc44af6
Kjamm <91775639+Kjamm@users.noreply.github.com>
2024-06-01 01:14:30 +0900

    Feat : 게임 보드 초기화 기능 추가
    - TableLayout과 TableRow를 사용하여 게임 보드 초기화

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,23 +1,22 @@
 package nextstep.omok
 
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var board: Board
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBoard()
+    }
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+    private fun setupBoard() {
+        val tableLayout = findViewById<TableLayout>(R.id.board)
+        board = Board(tableLayout, ::onCellClicked)
+        board.initialize()
     }
 }
