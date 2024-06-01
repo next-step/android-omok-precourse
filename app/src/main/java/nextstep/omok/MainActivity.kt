ca65f999db1338f28f571679e4474cb96312ec0f
SHEEN <normengdie@pusan.ac.kr>
2024-06-01 14:13:13 +0900

    모든 ImageView들을 FindView하는 createImageViewList 함수 생성

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,23 +1,37 @@
 package nextstep.omok
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val idLists = createImageViewList(this)
+        val boardSize = 15
+        val emptyBoard = MutableList(boardSize) { MutableList(boardSize) { 0 } }
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+
     }
+
+}
+fun createImageViewList(context: Context): List<ImageView> {
+    val ImageViewList = mutableListOf<ImageView>()
+
+    for (i in 1..255) {
+        val resourceId = context.resources.getIdentifier("button$i", "id", context.packageName)
+        val button = (context as? Activity)?.findViewById<ImageView>(resourceId)
+        button?.let { ImageViewList.add(it) }
+    }
+    return ImageViewList
 }
