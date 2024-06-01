4fb5ef7992068c7355ad3f1e14b172efdfe0596e
Kjamm <91775639+Kjamm@users.noreply.github.com>
2024-06-01 13:58:40 +0900

    feat : 게임 리셋 기능 구
    - 게임이 끝났을 때 보드를 초기 상태로 리셋하는 기능을 추가

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -2,11 +2,12 @@ package nextstep.omok
 
 import android.os.Bundle
 import android.widget.TableLayout
-import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var board: Board
+    private val gameLogic = GameLogic()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
