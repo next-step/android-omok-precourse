b4b6e515632588de02c366494909c1aba594bac1
khyeonm <122lamb@naver.com>
2024-06-01 19:18:19 +0900

    feat: Add onCellClicked

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    val boardSize = 15
+    val boardState = Array(boardSize) { Array(boardSize) { "" } }
     var currentPlayer = "w"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,7 +29,28 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    fun onCellClicked(view: ImageView, index: Int) {
+        val row = index / boardSize
+        val col = index % boardSize
+        if (boardState[row][col].isEmpty()) {
+            boardState[row][col] = currentPlayer
+            view.setImageResource(if (currentPlayer == "w") (R.drawable.white_stone) else (R.drawable.black_stone))
+            if (checkWin(row, col)) {
+                showWinner()
+            } else {
+                switchPlayer()
+            }
+        } else {
+            Toast.makeText(this, "이곳에는 놓을 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     fun switchPlayer() {
         currentPlayer = if (currentPlayer == "w") "b" else "w"
+    }
+    fun checkWin(): {
+
+    }
+    fun showWinner(): {
     }
 }
