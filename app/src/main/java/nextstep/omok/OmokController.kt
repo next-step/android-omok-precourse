30a9febd48b71aba289a5d9a0e8f1adb7a3d04fb
Jeong Suhyeon <jsh00325@naver.com>
2024-06-01 14:00:12 +0900

    refactor: Reduce Indentation Depth

--- app/src/main/java/nextstep/omok/OmokController.kt
+++ app/src/main/java/nextstep/omok/OmokController.kt
@@ -1,8 +1,6 @@
 package nextstep.omok
 
-class OmokController(
-	private val view: MainActivity
-) {
+class OmokController(private val view: MainActivity) {
 	private val board = OmokBoard()
 	private var curPlayer = Player.BLACK
 	private var isGameOver = false
@@ -12,28 +10,23 @@ class OmokController(
 	}
 
 	fun tryPutStone(r: Int, c: Int) {
-		if (isGameOver) return
+		if (!board.isCellAvailable(r, c) || isGameOver) return
 
-		if (board.isCellAvailable(r, c)) {
-			// 보드 정보 업데이트
-			board.putStone(r, c, curPlayer)
-			view.updateBoardCell(r, c, curPlayer)
+		// 보드 정보 업데이트
+		board.putStone(r, c, curPlayer)
+		view.updateBoardCell(r, c, curPlayer)
 
-			if (board.checkCurStoneIsWinner(r, c)) {
-				// 승리자 발생
-				view.showWinnerInfo(curPlayer)
-				isGameOver = true
-			}
-			else if (board.isDraw()) {
-				// 무승부
-				view.showWinnerInfo(Player.NONE)
-				isGameOver = true
-			}
-			else {
-				// 플레이어 변경
-				curPlayer = if (curPlayer == Player.BLACK) Player.WHITE else Player.BLACK
-				view.updateTurnInfo(curPlayer)
-			}
+		if (board.checkCurStoneIsWinner(r, c)) {
+			view.showWinnerInfo(curPlayer)
+			isGameOver = true
+		}
+		else if (board.isDraw()) {
+			view.showWinnerInfo(Player.NONE)
+			isGameOver = true
+		}
+		else {	// 승부가 나지 않음 -> 플레이어 변경
+			curPlayer = if (curPlayer == Player.BLACK) Player.WHITE else Player.BLACK
+			view.updateTurnInfo(curPlayer)
 		}
 	}
 }
