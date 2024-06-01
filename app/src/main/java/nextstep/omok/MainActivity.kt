f3cb26836f9e24cd5feb622cedcaa8ef4c5c0b94
leeghy <epuy109@gmail.com>
2024-06-01 16:51:59 +0900

    5줄 이상 연속으로 놓였는지 체크하는 기능(가로, 세로, 대각선 체크 함수)

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
     private var isBlackTurn = true
+    private lateinit var board: Array<Array<ImageView>>
+    private val boardSize = 15
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,24 +19,96 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setView() {
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener{ putStoneInTurn(view) } }
+        board = Array(boardSize) {Array(boardSize) { ImageView(this) } } // board라는 2차원 배열 생성
+        val tableLayout = findViewById<TableLayout>(R.id.board)
+
+        for (i in 0..boardSize) { // 각 셀에 접근, 행에 접근
+            val tableRow = tableLayout.getChildAt(i) as TableRow // i번째 자식 뷰 반환, tableRow로 캐스팅
+            for (j in 0..boardSize) { // 각 열에 접근
+                val imageView = tableRow.getChildAt(j) as ImageView // j번째 자식 뷰 반환, imageView로 캐스팅
+                board[i][j] = imageView
+                imageView.setOnClickListener { putStoneInTurn(i, j) }
+            }
+        }
     }
 
-    private fun putStoneInTurn(imageView: ImageView) {
-        if (imageView.tag == null) {
-            if (isBlackTurn) {
-                imageView.setImageResource(R.drawable.black_stone)
-            } else {
-                imageView.setImageResource(R.drawable.white_stone)
+    private fun putStoneInTurn(row: Int, col: Int) {
+        if (board[row][col].tag == null) { // 셀이 비어 있고
+            if (isBlackTurn) { // 흑돌 차례면
+                board[row][col].setImageResource(R.drawable.black_stone) // 흑돌을 놓는다
+            } else { // 백돌 차례면
+                board[row][col].setImageResource(R.drawable.white_stone) // 백돌을 놓는다
             }
-            imageView.tag = isBlackTurn
-            isBlackTurn = !isBlackTurn
+            board[row][col].tag = isBlackTurn // 흑돌이면 true, 백돌이면 false
+            isBlackTurn = !isBlackTurn // 차례 변경
+        }
+    }
+
+    private fun checkHorizontal(row: Int, color: Boolean): Boolean { // 가로 체크
+        var cnt = 0
+        for (col in 0..boardSize) { // 해당 셀의 행 검사
+            if (board[row][col].tag == color) { // 색깔이 같으면
+                cnt++ // 카운트 추가
+                if (cnt == 5) return true
+            } else {
+                cnt = 0
+            }
+        }
+        return false
+    }
+
+    private fun checkVertical(col: Int, color: Boolean): Boolean { // 세로 체크
+        var cnt = 0
+        for (row in 0..boardSize) {
+            if (board[row][col].tag == color) {
+                cnt++
+                if (cnt == 5) return true
+            } else {
+                cnt = 0
+            }
+        }
+        return false
+    }
+
+    private fun checkDiagonal (row: Int, col: Int, color: Boolean): Boolean {
+        var cnt = 0
+        var i = row
+        var j = col
+        while (i > 0 && j > 0) { // 대각선 시작점으로 이동
+            i--
+            j--
+        }
+        while (i < boardSize && j < boardSize) {
+            if (board[i][j].tag == color) {
+                cnt++
+                if (cnt == 5) return true
+            } else {
+                cnt = 0
+            }
+            i++
+            j++
+        }
+        return false
+    }
+    private fun checkReverseDiagonal (row: Int, col: Int, color: Boolean): Boolean {
+        var cnt = 0
+        var i = row
+        var j = col
+        while (i > 0 && j < boardSize - 1) {
+            i--
+            j++
+        }
+        while (i < boardSize && j >= 0) {
+            if (board[i][j].tag == color) {
+                cnt++
+                if (cnt == 5) return true
+            } else {
+                cnt = 0
+            }
+            i++
+            j--
+        }
+        return false
         }
     }
 }
