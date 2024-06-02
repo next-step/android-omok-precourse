508469fc71716ed9ffc91475381939a7825077ec
LeeMinSeo <real2ms@naver.com>
2024-06-02 14:31:48 +0900

    feat: player set stone and state update

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         val board = findViewById<TableLayout>(R.id.board)  //보드
         val restartButton = findViewById<Button>(R.id.restartButton)  //Restart Button
 
+        setBoard(board)  //보드의 각 칸에 클릭 리스너 설정
         /*
         board
             .children
@@ -29,5 +30,38 @@ class MainActivity : AppCompatActivity() {
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
             .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }*/
+    }
+
+    //보드의 각 칸에 클릭 리스너 설정
+    fun setBoard(board: TableLayout) {
+        board.children.filterIsInstance<TableRow>().forEachIndexed { rowIndex, row ->
+            row.children.filterIsInstance<ImageView>().forEachIndexed { colIndex, view ->
+                view.setOnClickListener {
+                    onCellClick(view, rowIndex, colIndex)  //칸(셀) 클릭 시 동작
+                }
+            }
+        }
+    }
+
+    //칸(셀) 클릭 시 동작 정의
+    fun onCellClick(view: ImageView, row: Int, col: Int) {
+        if (!gameEnded && boardState[row][col] == 0) {
+            placeStone(view, row, col)  //돌 놓기
+            handleGameState(row, col)  //게임 상태 처리
+        }
+    }
+
+    //돌 놓고 보드 상태 업데이트
+    fun placeStone(view: ImageView, row: Int, col: Int) {
+        val drawable = if (isBlackTurn) R.drawable.black_stone else R.drawable.white_stone
+        view.setImageResource(drawable)  //돌 이미지 설정
+        boardState[row][col] = if (isBlackTurn) 1 else 2  //보드 상태 업데이트
+    }
+
+    //게임 상태
+    fun handleGameState(row: Int, col: Int) {
+        when {
+
+        }
     }
 }
