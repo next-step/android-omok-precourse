5aa184b5d89daeb4660539c2bc9c3593866555df
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-06-03 02:50:43 +0900

    Feat : implement changeTurn()
    매 성공적인 착수마다 차례를 나타내는 프로퍼티인 turn을 바꾸는 changeTurn() 구현

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,23 +1,41 @@
 package nextstep.omok
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
+const val BOARD_SIZE: Int = 15
+
 class MainActivity : AppCompatActivity() {
+    private var turn: User = User.BLACK
+    private var boards = Array(BOARD_SIZE) { Array<BoardState>(BOARD_SIZE) { BoardState.EMPTY } }
+    private fun changeTurn() {
+        this.turn = if (turn == User.BLACK) User.WHITE else User.BLACK
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val board = findViewById<TableLayout>(R.id.board)
+
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { idx, view ->
+                view.setOnClickListener {
+
+                    changeTurn()
+
+                }
+            }
     }
 }
