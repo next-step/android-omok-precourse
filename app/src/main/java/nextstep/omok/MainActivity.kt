10b0f8a8bc81ab33f494ac09b7b725e5447a9b0b
이지은 <jieun7056@naver.com>
2024-06-02 11:13:45 +0900

    feat: 돌을 번갈아 놓기

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private var turn = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +19,30 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view -> view.setOnClickListener {
+                placeStone(view)
+                checkWin()
+                checkGameOver()
+            } }
+
     }
+    private fun placeStone(view: ImageView) {
+        if (view.drawable != null) return
+
+        val stone = if (turn % 2 == 0) R.drawable.black_stone else R.drawable.white_stone
+        view.setImageResource(stone)
+
+        addTurn()
+    }
+
+    private fun addTurn() {
+        turn++
+    }
+
+    private fun checkWin() {
+    }
+
+    private fun checkGameOver() {
+    }
+
 }
