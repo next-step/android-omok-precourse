1266275b0925d681059f7756863cfa330c45267e
LeeMinSeo <real2ms@naver.com>
2024-06-02 14:14:08 +0900

    feat: set variables

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -11,16 +11,23 @@ import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
 
+    var isBlackTurn = true  //플레이어1(흑돌) or 플레이어2(백돌) 여부
+    val boardState = Array(15) { IntArray(15) {0} }  //보드 상태(2차원 배열), 0: 빈 칸, 1: 흑돌, 2: 백돌
+    var gameEnded = false  //게임 종료 여부
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
+        val board = findViewById<TableLayout>(R.id.board)  //보드
+        val restartButton = findViewById<Button>(R.id.restartButton)  //Restart Button
+
+        /*
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }*/
     }
 }
