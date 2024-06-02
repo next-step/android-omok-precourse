0cd50bb7487fe745fc04995648ef14119d55cdf3
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-06-03 03:03:57 +0900

    Feat : Check win condition
    착수 후 승리 여부를 판단하는 isWin() 구현.
    checkDirection으로 각 방향(수직, 수평)을 지정하고
    countDirection으로 선택된 방향의 정,역을 방문(수평시 좌,우)

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.omok
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
@@ -23,7 +24,55 @@ class MainActivity : AppCompatActivity() {
     private fun isEmptySpace(idx: Int): Boolean {
         return boards[idx / BOARD_SIZE][idx % BOARD_SIZE] == BoardState.EMPTY
     }
-    
+
+    // 각 방향에 승리 조건(연속된 5개 이상의 같은 돌)이 있는지 판별한다.
+    private fun isWin(idx: Int): Boolean {
+        val col = idx / BOARD_SIZE
+        val row = idx % BOARD_SIZE
+
+        return checkDirection(col, row, 1, 0) ||  // 수직
+                checkDirection(col, row, 0, 1) ||  // 수평
+                checkDirection(col, row, 1, 1) ||  // 대각선 /
+                checkDirection(col, row, 1, -1)    // 대각선 \
+    }
+
+    // 각 방향(수평, 수직, 대각선)에 해당하는 두가지 방향(수평일 시 좌, 우)에 5개 이상의 같은 돌이 있는지 확인한다.
+    // col, row는 착수한 돌의 행, 열 인덱스이며 colStep, rowStep은 각 행,열로의 이동값이다.
+    // target은 착수한 돌의 boardState이며 count는 연속된 같은 돌의 개수이다.
+    private fun checkDirection(col: Int, row: Int, colStep: Int, rowStep: Int): Boolean {
+        val target = boards[col][row]
+        var count = 1
+
+        count += countDirection(col, row, colStep, rowStep, target)
+        count += countDirection(col, row, -colStep, -rowStep, target)
+
+        return count >= 5
+    }
+
+    //boards를 방문하여 target(착수한 돌)과 연속된 같은 돌의 개수를 반환한다
+    private fun countDirection(
+        col: Int,
+        row: Int,
+        colStep: Int,
+        rowStep: Int,
+        target: BoardState
+    ): Int {
+        var count = 0
+        var currentCol = col + colStep
+        var currentRow = row + rowStep
+
+        while (true) {
+            if (currentCol < 0 || currentCol >= BOARD_SIZE) break
+            if (currentRow < 0 || currentRow >= BOARD_SIZE) break
+            if (boards[currentCol][currentRow] != target) break
+            count++
+            currentCol += colStep
+            currentRow += rowStep
+        }
+
+        return count
+    }
+
     private fun placeStoneView(view: ImageView) {
         val stone = if (turn == User.BLACK) R.drawable.black_stone else R.drawable.white_stone
         view.setImageResource(stone)
@@ -38,12 +87,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
-
+    private fun initListensers(board: TableLayout) {
         board
             .children
             .filterIsInstance<TableRow>()
@@ -57,5 +102,14 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val board = findViewById<TableLayout>(R.id.board)
+
+        initListensers(board)
     }
 }
