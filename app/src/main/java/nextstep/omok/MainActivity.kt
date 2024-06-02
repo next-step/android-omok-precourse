1f791ddf802f3234d741e88dd30f53f77b7be060
ajang369 <jhjun99421@gmail.com>
2024-06-03 00:41:06 +0900

    feat: MainActivity에 게임 구현
    Board 클래스와 GameManager 클래스를 활용하여 게임 구현
    TableRow를 x축, ImageView를 y축으로 하여 각 index 값을 사용하여 클릭 시 돌을 놓는 기능과 승자를 확인하는 조건문으로 오목을 구현함

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -4,20 +4,50 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    // 객체 필드 변수
+    private lateinit var omokBoard: Board
+    private lateinit var gameManager: GameManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var size = 15
+        omokBoard = Board(15)
+        gameManager = GameManager(omokBoard)
 
         val board = findViewById<TableLayout>(R.id.board)
         board
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { rowIdx, rowBoard ->
+                rowBoard
+                    .children
+                    .filterIsInstance<ImageView>()
+                    .forEachIndexed { colIdx, view ->
+                        view.setOnClickListener {
+                            if (omokBoard.checkPlaceStone(rowIdx, colIdx, gameManager.checkTurn())) {
+                                view.setImageResource(
+                                    if (gameManager.checkTurn() == 1) R.drawable.black_stone
+                                    else R.drawable.white_stone
+                                )
+
+                                if (gameManager.checkWinner(rowIdx, colIdx, gameManager.checkTurn())) {
+                                    // 게임 종료
+                                    Toast.makeText(this, "플레이어${gameManager.checkTurn()} 승리", Toast.LENGTH_LONG).show()
+                                } else {
+                                    gameManager.switchTurn()
+                                }
+                            }
+                        }
+                    }
+
+            }
+
     }
 }
