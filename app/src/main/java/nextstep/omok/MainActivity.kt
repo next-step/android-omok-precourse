0de164a9ad1c8c0065586bdac1f58a23fc1e3f7f
anyooin <anyooin123@naver.com>
2024-06-03 00:51:21 +0900

    feat : putStone

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.omok
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -12,12 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val blackPlayer = Player("black")
+        val whitePlayer = Player("white")
+
         val board = findViewById<TableLayout>(R.id.board)
         board
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view -> view.setOnClickListener {
+                blackPlayer.putStone(view)
+            } }
+    }
+}
+
+class Player(
+    val color : String
+) {
+    fun putStone(view : ImageView) {
+        if(color == "black") {
+            view.setImageResource(R.drawable.black_stone)
+        } else {
+            view.setImageResource(R.drawable.white_stone)
+        }
+        view.isClickable = false // 이미 돌을 둔 칸은 못두도록
     }
 }
