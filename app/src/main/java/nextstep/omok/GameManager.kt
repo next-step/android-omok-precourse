42a24db09ad64dc263fa8c6975d20185661a1184
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-03 10:55:17 +0900

    feat(GameManager): GameManager 클래스 placeStone(row, col)함수 구현

--- app/src/main/java/nextstep/omok/GameManager.kt
+++ app/src/main/java/nextstep/omok/GameManager.kt
@@ -1,15 +1,43 @@
 package nextstep.omok
 
+//class GameManager {
+//    private val boardSize = 15
+//    private val board: Array<Array<Int>> = Array(boardSize) { Array(boardSize) { 0 } }
+//    private var isBlackTurn = true
+//    fun getBoard(): Array<Array<Int>> = board
+//    fun getCurrentStone(): Int {
+//        return if (isBlackTurn) R.drawable.black_stone else R.drawable.white_stone
+//    }
+//    fun switchTurn() {
+//        isBlackTurn = !isBlackTurn
+//    }
+//}
+
 class GameManager {
-    private val boardSize = 15
-    private val board: Array<Array<Int>> = Array(boardSize) { Array(boardSize) { 0 } }
+    private val BOARD_SIZE = 15
+    private val board: Array<Array<Int>> = Array(BOARD_SIZE) { Array(BOARD_SIZE) { 0 } }
     private var isBlackTurn = true
+
     fun getBoard(): Array<Array<Int>> = board
+
+    fun placeStone(row: Int, col: Int): Int {
+        if (board[row][col] == 0) {
+            board[row][col] = getCurrentStone()
+            switchTurn()
+            return board[row][col]
+        }
+        return 0
+    }
+
     private fun getCurrentStone(): Int {
         return if (isBlackTurn) R.drawable.black_stone else R.drawable.white_stone
     }
 
     private fun switchTurn() {
         isBlackTurn = !isBlackTurn
+    }
+
+    fun checkWin(): Boolean {
+        return false
     }
 }
