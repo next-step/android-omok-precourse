bb61e8d49ca44237e4301929b257ace455b956ce
fiveJinw <fiveJinw1124@gmail.com>
2024-06-03 11:57:57 +0900

    feat: 클릭한 위치를 반환하는 기능

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -8,16 +8,32 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    lateinit var board :TableLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
+        board = findViewById<TableLayout>(R.id.board)
+        boardOnClickListener()
+    }
+
+    fun boardOnClickListener(){
+        board.children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEach { view ->
+                view.setOnClickListener {
+                    val position = getClickPosition(view)
+                }
+            }
+    }
+
+    fun getClickPosition(view : ImageView) : List<Int>{
+        val row = view.parent as TableRow
+        val rowIndex = row.indexOfChild(view)
+        val columnIndex = board.indexOfChild(row)
+        return listOf(rowIndex, columnIndex)
     }
 }
