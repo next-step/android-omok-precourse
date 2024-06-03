132b4c8cf458e4c031823fe21714e30e38bcc409
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-03 12:16:08 +0900

    feat(MainActivity): 메인 액티비티에 사용자가 우승한 경우 재시작 할 수 있도록 showReGameAlert()함수 구현, 보드를 초기화하는 resetBoard()함수 구현

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,10 +1,12 @@
 package nextstep.omok
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 class MainActivity : AppCompatActivity() {
@@ -48,12 +50,46 @@ class MainActivity : AppCompatActivity() {
                     colIndex
                 ) as ImageView
             imageView.setImageResource(stone)
-        } else {
+            checkWin(gameManager.checkWin(stone, rowIndex, colIndex), stone)
+        } else { // 돌이 이미 있는 경우
             showToast()
         }
     }
-    
+
     private fun showToast() {
         Toast.makeText(this, "잘못된 위치!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showReGameAlert(stone: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(""+gameManager.getStoneName(stone)+" 우승!")
+        builder.setMessage("게임 재시작?")
+        builder.setPositiveButton("예", DialogInterface.OnClickListener { dialog, which ->
+            gameManager.resetGame()
+            resetBoard()
+        })
+        builder.setNegativeButton("아니오", DialogInterface.OnClickListener { dialog, which ->
+            finish()
+        })
+        builder.create()
+        builder.show()
+    }
+
+    private fun checkWin(isWon: Boolean, stone: Int) {
+        if (isWon) {
+            showReGameAlert(stone)
+        }
+    }
+
+    private fun resetBoard() {
+        val size = gameManager.getSize()
+        val boardLayout = findViewById<TableLayout>(R.id.board)
+        for (i in 0 until size) {
+            val row = boardLayout.getChildAt(i) as TableRow
+            for (j in 0 until size) {
+                val imageView = row.getChildAt(j) as ImageView
+                imageView.setImageResource(0) // 이미지 초기화
+            }
+        }
     }
     }
