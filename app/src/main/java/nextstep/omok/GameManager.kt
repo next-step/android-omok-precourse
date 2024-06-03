9ea9f2a73bdc1d046f9f76cb13b40e804d8f4941
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-03 12:12:21 +0900

    feat(GameManager): 사용자의 돌이 5개 이상 이어져 있는지 알기 위한 checkWin(), isVLinked(), isHLinked(), isLRDiagLinked(), isRLDiagLinked() 메서드 기능 구현

--- app/src/main/java/nextstep/omok/GameManager.kt
+++ app/src/main/java/nextstep/omok/GameManager.kt
@@ -1,29 +1,19 @@
 package nextstep.omok
 
-//class GameManager {
-//    private val boardSize = 15
-//    private val board: Array<Array<Int>> = Array(boardSize) { Array(boardSize) { 0 } }
-//    private var isBlackTurn = true
-//    fun getBoard(): Array<Array<Int>> = board
-//    fun getCurrentStone(): Int {
-//        return if (isBlackTurn) R.drawable.black_stone else R.drawable.white_stone
-//    }
-//    fun switchTurn() {
-//        isBlackTurn = !isBlackTurn
-//    }
-//}
+import android.util.Log
 
 class GameManager {
     private val BOARD_SIZE = 15
     private val board: Array<Array<Int>> = Array(BOARD_SIZE) { Array(BOARD_SIZE) { 0 } }
     private var isBlackTurn = true
-
-    fun getBoard(): Array<Array<Int>> = board
+    val blackStone = R.drawable.black_stone
+    fun getSize(): Int = BOARD_SIZE
 
     fun placeStone(row: Int, col: Int): Int {
         if (board[row][col] == 0) {
             board[row][col] = getCurrentStone()
             switchTurn()
+            Log.d("stone", "ID: "+board[row][col])
             return board[row][col]
         }
         return 0
@@ -37,7 +27,97 @@ class GameManager {
         isBlackTurn = !isBlackTurn
     }
 
-    fun checkWin(): Boolean {
-        return false
+    fun getStoneName(stone: Int): String {
+        if (stone==blackStone) return "흑돌"
+        else return "백돌"
+    }
+    fun checkWin(id: Int, row: Int, col: Int): Boolean {
+        if(isVLinked(id, row, col)||isHLinked(id, row, col)||isRLDiagLinked(id, row, col)||isLRDiagLinked(id,row,col)) return true
+        else return false
+    }
+
+    private fun isVLinked(id: Int, row: Int, col: Int): Boolean {
+        var count: Int = 1
+        for (i in row - 1 downTo 0) { // 위
+            if (board[i][col] == id) {
+                count++
+            } else {
+                break
+            }
+        }
+        for (i in row + 1 until board.size) { // 아래
+            if (board[i][col] == id) {
+                count++
+            } else {
+                break
+            }
+        }
+        return count >= 5 // 5개 이상의 돌이 연결되어 있으면 true
+    }
+
+    private fun isHLinked(id: Int, row: Int, col: Int): Boolean { // 좌우 체크
+        var count: Int = 1
+        for (i in col - 1 downTo 0) { // 오른쪽
+            if (board[row][i] == id) {
+                count++
+            } else {
+                break
+            }
+        }
+        for (i in col + 1 until board.size) { // 오른쪽
+            if (board[row][i] == id) {
+                count++
+            } else {
+                break
+            }
+        }
+        return count >= 5 // 5개 이상의 돌이 연결되어 있으면 true
+    }
+
+    private fun isLRDiagLinked(id: Int, row: Int, col: Int): Boolean {
+        var count = 1
+
+        var i = row - 1 // 왼쪽 위 대각선
+        var j = col - 1
+        while (i >= 0 && j >= 0 && board[i][j] == id) {
+            count++
+            i--
+            j--
+        }
+
+        i = row + 1 // 오른쪽 아래 대각선
+        j = col + 1
+        while (i < board.size && j < board.size && board[i][j] == id) {
+            count++
+            i++
+            j++
+        }
+        return count >= 5
+    }
+
+    private fun isRLDiagLinked(id: Int, row: Int, col: Int): Boolean {
+        var count = 1
+
+        var i = row - 1 // 왼쪽 위 대각선
+        var j = col + 1
+        while (i >= 0 && j < board.size && board[i][j] == id) {
+            count++
+            i--
+            j++
+        }
+
+        i = row + 1 // 오른쪽 아래 대각선
+        j = col - 1
+        while (i < board.size && j >= 0 && board[i][j] == id) {
+            count++
+            i++
+            j--
+        }
+        return count >= 5
+    }
+
+    fun resetGame() {
+        board.forEach { row -> row.fill(0) }
+        isBlackTurn = true
     }
 }
