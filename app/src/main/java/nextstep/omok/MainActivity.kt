4cc26e1b21d31861a503e697d71a0aff869a9343
graccon <sujin.noh01@gmail.com>
2024-06-03 15:09:15 +0900

    feat: 게임판 초기화 및 돌 놓기 인터페이스 설정
    - 게임판을 구성하고 초기 상태로 설정
    - 사용자가 돌을 놓을 수 있는 클릭 리스너

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,6 +1,7 @@
 package nextstep.omok
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
@@ -8,16 +9,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    val boardSize = 15
+    lateinit var board: TableLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
+        board = findViewById(R.id.board)
+        initializeBoard(board)
+    }
+
+    private fun initializeBoard(board: TableLayout) {
         board
             .children
             .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { rowIndex, tableRow ->
+                tableRow.children
+                    .filterIsInstance<ImageView>()
+                    .forEachIndexed { colIndex, imageView ->
+                        imageView.tag = Pair(rowIndex, colIndex)
+                        imageView.setOnClickListener { onCellClick(imageView) }
+                    }
+            }
+    }
+
+    private fun onCellClick(cell: ImageView) {
+        // 태그에서 좌표 정보를 추출하여 로그 출력
+        val position = cell.tag as Pair<Int, Int>
+        Log.d("testt", "Row: ${position.first}, Column: ${position.second}")
     }
 }
