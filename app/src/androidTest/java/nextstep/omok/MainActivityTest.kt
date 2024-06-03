76450a4a873100ece5bb4399db3dd5497896db43
kimdo-7275 <66244658+kimdo-7275@users.noreply.github.com>
2024-06-03 15:49:51 +0900

    testcase

--- app/src/androidTest/java/nextstep/omok/MainActivityTest.kt
+++ app/src/androidTest/java/nextstep/omok/MainActivityTest.kt
@@ -1,9 +1,48 @@
 package nextstep.omok
 
-import org.junit.jupiter.api.Assertions.*
-
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+    private lateinit var activity: MainActivity
 
+    @BeforeEach
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity {
+            activity = it
+        }
+    }
+    @Test
+    // 흑돌 세로로 놓았을 때 승리조건 확인
+    fun testCountStones() {
+        activity.runOnUiThread {
+            activity.stoneList[0][0] = 1
+            activity.stoneList[0][1] = 1
+            activity.stoneList[0][2] = 1
+            activity.stoneList[0][3] = 1
+            activity.stoneList[0][4] = 1
+
+
+            assertEquals(true, activity.checkForWin(0, 2))
+        }
+    }
+     //백돌을 대각선으로 놓았을때
+    @Test
+    fun testCountStonesDiagonal() {
+        activity.runOnUiThread {
+            activity.stoneList[0][0] = 2
+            activity.stoneList[1][1] = 2
+            activity.stoneList[2][2] = 2
+            activity.stoneList[3][3] = 2
+            activity.stoneList[4][4] = 2
+
+
+            assertEquals(true, activity.checkForWin(0, 0))
+        }
+    }
 }
