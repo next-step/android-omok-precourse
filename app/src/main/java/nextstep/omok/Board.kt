e7958dc668fb669abd671404cf2521605219b9f4
fiveJinw <fiveJinw1124@gmail.com>
2024-06-03 11:48:11 +0900

    feat: 판 상태판단 기능 구현

--- app/src/main/java/nextstep/omok/Board.kt
+++ app/src/main/java/nextstep/omok/Board.kt
@@ -9,4 +9,22 @@ class Board {
             for (j in 0..<MAX_SIZE)
                 board[i][j] = 0
     }
+    fun checkIndex(row: Int, col:Int):Boolean{
+        return if(row < 0 || row >= MAX_SIZE || col < 0 || col >= MAX_SIZE){
+            print("nullError" + "인덱스 오류")
+            false
+        } else true
+    }
+
+    fun isEmpty(row: Int, col: Int): Boolean? {
+        return if (checkIndex(row, col)) board[row][col] == 0
+        else null
+    }
+
+    fun checkStone(row : Int, col : Int) : Int?{
+        return if (checkIndex(row, col)) board[row][col]
+        else null
+    }
+
+
 }
