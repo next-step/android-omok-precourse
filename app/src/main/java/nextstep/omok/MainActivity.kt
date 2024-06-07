39d654d096ea87687d03bc6f3ac67f9b65e7b97f
SHEEN <normengdie@pusan.ac.kr>
2024-06-07 10:04:54 +0900

    클릭 이벤트를 위한 afterClick 함수 구현

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -7,7 +7,8 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+var turn = 1
+val board = MutableList(15) { MutableList(15) { 0 } }
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,5 +33,14 @@ fun imageViewClick(idLists: List<ImageView>, whosTurn: TextView){
         afterClick(k, idLists,whosTurn)
     }
 
+}
+fun afterClick(k: Int, idLists: List<ImageView>, whosTurn: TextView) {
+    if (k < 255) {
+        val clickView = idLists[k]
+        clickView.setOnClickListener {
+            createStone(clickView,k,whosTurn)
+
+        }
+    }
 }
 
