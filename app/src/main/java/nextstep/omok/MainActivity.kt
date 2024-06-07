4619a4defe092c23a1489e15d63420a80af10c29
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-06-07 21:10:04 +0900

    MainActivity.kt - 필요한 변수 및 함수 선언

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,23 +1,40 @@
 package nextstep.omok
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
+    private var turn = 0
+    private var table = Array(15) { IntArray(15) { 0 } }
+    val board = findViewById<TableLayout>(R.id.board)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+        setupBoard()
     }
+
+    private fun setupBoard() {}
+
+    private fun placeStone() {}
+
+    private fun updateBoardState() {}
+
+    private fun countStones() {}
+
+    private fun countDirection() {}
+
+    private fun checkWin() {}
+
+    private fun handleWin() {}
+
+    private fun showWinDialog() {}
+
 }
+
