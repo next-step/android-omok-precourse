36c12bf8f7a127e924b5fe6808308ab156469250
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-06-07 22:10:48 +0900

    게임 종료 Dialog 구현 - 매 턴마다 승리 감지 로직이 작동한 결과로 true를 리턴하면 게임 종료 - Yes 클릭 시: 현재 액티비티 끝내고 액티비티 재실행(intent 이용) - No 클릭 시: 액티비티 종료

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var turn = 0
     private val boardSize = 15
     private var table = Array(boardSize) { IntArray(boardSize) { 0 } }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,20 +34,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getStoneResource(): Int = if (turn % 2 == 0) R.drawable.black_stone else R.drawable.white_stone
-
     fun placeStone(view: ImageView, row: Int, col: Int) {
         if (table[row][col] == 0) {
             val stoneResource = getStoneResource()
             view.setImageResource(stoneResource)
             updateBoardState(row, col)
+            if (checkWin(row, col)) handleWin()
         }
     }
+
+    private fun getStoneResource(): Int = if (turn % 2 == 0) R.drawable.black_stone else R.drawable.white_stone
 
     private fun updateBoardState(row: Int, col: Int) {
         table[row][col] = if (turn % 2 == 0) 1 else 2
         turn++
     }
+
     private fun countStones(row: Int, col: Int, dx: Int, dy: Int): Int {
         return 1 + countDirection(row, col, dx, dy) + countDirection(row, col, -dx, -dy)
     }
@@ -67,11 +71,27 @@ class MainActivity : AppCompatActivity() {
         return directions.any { countStones(row, col, it[0], it[1]) >= 5 }
     }
 
-    private fun handleWin() {}
+    private fun handleWin() {
+        val winner = getWinner()
+        showWinDialog(winner)
+    }
 
-    private fun showWinDialog() {}
+    private fun getWinner() = if (turn % 2 == 1) "흑" else "백"
 
+    private fun showWinDialog(winner: String) {
+        runOnUiThread {
+            AlertDialog.Builder(this@MainActivity).apply {
+                setTitle("게임 종료")
+                setMessage("${winner}이 승리했습니다. 새 게임을 진행하시겠습니까?")
+                setupDialogButtons()
+                setCancelable(false)
+                show()
+            }
+        }
+    }
+
+    private fun AlertDialog.Builder.setupDialogButtons() {
+        setNegativeButton("No") { _,_ -> finish() }
+        setPositiveButton("Yes") { _,_ -> finish(); startActivity(intent) }
+    }
 }
-
-
-
