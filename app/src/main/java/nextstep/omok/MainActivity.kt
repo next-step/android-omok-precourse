d08076babf4b744be50dc99c94d024e15e424f4b
SHEEN <normengdie@pusan.ac.kr>
2024-06-07 10:01:33 +0900

    FindViewId를 위한 createImageViewList 함수 구현

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -1,23 +1,28 @@
 package nextstep.omok
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
-import android.widget.TableLayout
-import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val board = findViewById<TableLayout>(R.id.board)
-        board
-            .children
-            .filterIsInstance<TableRow>()
-            .flatMap { it.children }
-            .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+
     }
+}
+fun createImageViewList(context: Context): List<ImageView> {
+    val ImageViewList = mutableListOf<ImageView>()
+
+    for (i in 1..255) {
+        val resourceId = context.resources.getIdentifier("button$i", "id", context.packageName)
+        val image = (context as? Activity)?.findViewById<ImageView>(resourceId)
+        image?.let { ImageViewList.add(it) }
+    }
+    return ImageViewList
 }
