f847413469652738135ffee0916d4a6b70cd06b8
sun <50648835+kimseongyu@users.noreply.github.com>
2024-06-09 05:08:40 +0900

    Feat: Define constant value

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -7,6 +7,19 @@ import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
+const val BOARDSIZE = 15
+
+const val EMPTY = 0
+const val BLACK = 1
+const val WHITE = 2
+const val TIE = 3
+
+const val WINCOUNT = 5
+
+val derivative = arrayOf(
+    arrayOf(1, 0), arrayOf(0, 1), arrayOf(1, 1), arrayOf(1, -1)
+)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
