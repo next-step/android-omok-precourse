8f7fb5d6eafa57dbc4240faeafca64e38315aa22
aengzu <jeon7879@naver.com>
2024-06-08 23:58:14 +0900

    feat: create game board view

---
+++ app/src/main/java/nextstep/omok/BoardActivity.kt
@@ -0,0 +1,15 @@
+package nextstep.omok
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+
+    }
+}
