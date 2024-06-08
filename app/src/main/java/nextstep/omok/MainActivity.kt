8f1b3ff04c2dc4cd148ef6e7ff95bfbc1db52536
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-08 15:23:30 +0900

    feat: Add isPositionEmpty function
    Allow placing stones only on empty spaces

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
+
 class MainActivity : AppCompatActivity() {
 
-    private var currentStone = "b"  //black
-    private val boardSate = Array(15) { Array<String?>(15) { null } }
+    private var currentStone = 0  //0:black, 1:white
+
+    private val boardSize = 15
+    private val boardState = Array<Int?>(boardSize * boardSize) { null }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +30,31 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { placeStone(view) } }
+            .forEachIndexed { index, view ->
+                view.setOnClickListener { isPositionEmpty(view, index) }
+            }
     }
 
-    private fun placeStone(view:ImageView) {
-        if (currentStone == "b") {
+    private fun isPositionEmpty(view: ImageView, position: Int) {
+        val row = position / boardSize
+        val column = position % boardSize
+
+        if (boardState[position] == null) {
+            placeStone(view,position)
+        } else {
+            val toast = Toast.makeText(applicationContext, "이미 돌이 놓인 위치입니다.", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+    private fun placeStone(view:ImageView,position:Int) {
+        if (currentStone == 0) {
             view.setImageResource(R.drawable.black_stone)
-            currentStone = "w"
+            boardState[position] = 0;   //black
+            currentStone = 1
         } else {
         view.setImageResource(R.drawable.white_stone)
-        currentStone = "b"}
+        boardState[position] = 1;   //white
+        currentStone = 0}
     }
 }
