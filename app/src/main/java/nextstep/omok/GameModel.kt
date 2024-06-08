8dca8e6bde872411e2d0e21abf3b48a5f97de5e1
aengzu <jeon7879@naver.com>
2024-06-08 23:16:34 +0900

    feat: add basic structure of game model

--- app/src/main/java/nextstep/omok/GameModel.kt
+++ app/src/main/java/nextstep/omok/GameModel.kt
@@ -1,4 +1,24 @@
 package nextstep.omok
 
 class GameModel {
+    var currentPlayer: Player = Player.BLACK // 우선 흑돌로 현재 플레이어 초기화
+
+    fun resetGame() {
+        // TODO: 게임 초기화 로직
+    }
+    fun switchPlayer() {
+        // TODO: 플레이어 전환 로직
+    }
+    fun checkWinCondition() {
+        // TODO: 승리 조건 확인 로직
+    }
+    fun placeStone() {
+        // TODO: 현재 플레이어가 돌을 놓는 로직
+    }
+
+    fun handelWin() {
+        // TODO : 승리 조건이 만족되었을 때 처리 로직
+    }
+
+
 }
