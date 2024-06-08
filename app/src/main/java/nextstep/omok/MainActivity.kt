bc25ee4f82f323a61fa288c42d655d24f6e10d62
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-09 00:54:42 +0900

    Refactor: Change functions to public for test code

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         initializeBoard(board)
     }
 
-    private fun initializeBoard(board: TableLayout){
+    fun initializeBoard(board: TableLayout){
         board
             .children
             .filterIsInstance<TableRow>()
@@ -35,16 +35,16 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun isPositionEmpty(view: ImageView, position: Int) {
+    fun isPositionEmpty(view: ImageView, position: Int) {
         if (boardState[position] == null)  placeStone(view,position)
         else showToast("이미 돌이 놓인 곳입니다.\n 다른 위치를 선택하세요.")
     }
 
-    private fun showToast(message: String) {
+    fun showToast(message: String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun placeStone(view: ImageView,position: Int) {
+    fun placeStone(view: ImageView,position: Int) {
         if (currentStone == 0) {
             view.setImageResource(R.drawable.black_stone)
             boardState[position] = 0   //black
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         if (checkState(position)) {displayResult()}
     }
 
-    private fun checkState(position: Int): Boolean{
+    fun checkState(position: Int): Boolean{
         val row = position / boardSize
         val column = position % boardSize
         val stone = boardState[position]?: return false
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         return checkVertical(row,column,stone) || checkHorizontal(row,column,stone) || checkDiagonal(row,column,stone)
     }
 
-    private fun checkVertical(row: Int, column: Int, stone: Int): Boolean{
+    fun checkVertical(row: Int, column: Int, stone: Int): Boolean{
         val topCount = (row - 1 downTo 0).takeWhile { boardState[it * boardSize + column] == stone }.count()
         val bottomCount = (row + 1 until boardSize).takeWhile { boardState[it * boardSize + column] == stone }.count()
 
@@ -74,7 +74,7 @@ class MainActivity : AppCompatActivity() {
         return (count >= 4)
     }
 
-    private fun checkHorizontal(row: Int, column: Int, stone: Int): Boolean{
+    fun checkHorizontal(row: Int, column: Int, stone: Int): Boolean{
 
         val leftCount = (column - 1 downTo 0).takeWhile { boardState[row * boardSize + it] == stone }.count()
         val rightCount = (column + 1 until boardSize).takeWhile { boardState[row * boardSize + it] == stone }.count()
@@ -84,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         return (count >= 4)
     }
 
-    private fun checkDiagonal(row: Int, column: Int, stone: Int): Boolean{
+    fun checkDiagonal(row: Int, column: Int, stone: Int): Boolean{
         // 왼->오 대각선
         val leftTopCount = (1..minOf(row, column)).takeWhile { boardState[(row - it) * boardSize + (column - it)] == stone }.count()
         val rightBottomCount = (1..minOf(boardSize - row - 1, boardSize - column - 1)).takeWhile { boardState[(row + it) * boardSize + (column + it)] == stone }.count()
@@ -99,8 +99,8 @@ class MainActivity : AppCompatActivity() {
         return (countd1>=4 || countd2>=4)
     }
 
-    private fun displayResult() {
-        
+    fun displayResult() {
+
         if (currentStone == 1) {
             showToast("흑돌 승리! \n게임이 종료됩니다!")
             finish()
