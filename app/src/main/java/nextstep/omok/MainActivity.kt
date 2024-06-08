7b588f0f528cca5b4b979f74a2928edeeca7dff3
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-08 16:54:27 +0900

    Refactor: Add showToast function
    Extract part of isPositionEmpty into a new function

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private val boardSize = 15
     private val boardState = Array<Int?>(boardSize * boardSize) { null }
+    //private val boardState = ArrayList<Int?>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         initializeBoard(board)
     }
 
-    private fun initializeBoard(board:TableLayout){
+    private fun initializeBoard(board: TableLayout){
         board
             .children
             .filterIsInstance<TableRow>()
@@ -36,18 +37,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun isPositionEmpty(view: ImageView, position: Int) {
-        val row = position / boardSize
-        val column = position % boardSize
-
-        if (boardState[position] == null) {
-            placeStone(view,position)
-        } else {
-            val toast = Toast.makeText(applicationContext, "이미 돌이 놓인 위치입니다.", Toast.LENGTH_SHORT)
-            toast.show()
-        }
+        if (boardState[position] == null)  placeStone(view,position)
+        else showToast("이미 돌이 놓인 곳입니다.\n 다른 위치를 선택하세요.")
     }
 
-    private fun placeStone(view:ImageView,position:Int) {
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun placeStone(view: ImageView,position: Int) {
         if (currentStone == 0) {
             view.setImageResource(R.drawable.black_stone)
             boardState[position] = 0;   //black
@@ -56,5 +54,10 @@ class MainActivity : AppCompatActivity() {
         view.setImageResource(R.drawable.white_stone)
         boardState[position] = 1;   //white
         currentStone = 0}
+
+        if (boardState.filterNotNull().size >= 3){
+            //checkWin(position)
+        }
     }
+
 }
