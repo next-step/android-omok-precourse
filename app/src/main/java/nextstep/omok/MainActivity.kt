a1ac1286e0b2c3066a413465a9473a55224d5b19
ddangcong80 <ddangcong80@gmail.com>
2024-06-10 01:19:14 +0900

    feat: 오목판 초기화 기능 구현

--- app/src/main/java/nextstep/omok/MainActivity.kt
+++ app/src/main/java/nextstep/omok/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 
+val derivative = arrayOf(
+    arrayOf(1, 0), arrayOf(0, 1), arrayOf(1, 1), arrayOf(1, -1)
+)
+
 class MainActivity : AppCompatActivity() {
+    private val boardSize: Int = 15;
+    private var player: String = "black"
+    private var winnerPlayer: String = ""
+    private var gameOver: Boolean = false
+    private val boardState = Array(boardSize) { Array(boardSize) { "" } }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +29,31 @@ class MainActivity : AppCompatActivity() {
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
             .filterIsInstance<ImageView>()
-            .forEach { view -> view.setOnClickListener { view.setImageResource(R.drawable.black_stone) } }
+            .forEachIndexed { index, view ->
+                view.setOnClickListener { onCellClicked(index, view) }
+            }
+
     }
+
+    fun onCellClicked(index: Int, view: ImageView) {
+        if (gameOver) {
+            return
+        }
+
+        val row: Int = index / boardSize
+        val col: Int = index % boardSize
+        if (boardState[row][col] == "") {
+            placeStone(view)
+            boardState[row][col] = player
+            if (checkGameSet(row, col)) {
+                winnerPlayer = player
+                showWinner()
+            } else if (isBoardFull()) {
+                showDraw()
+            } else {
+                changePlayer()
+            }
+        }
+    }
+
 }
